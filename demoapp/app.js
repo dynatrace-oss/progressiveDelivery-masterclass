62a2668979970c13d27938228c6f16ad88e68d78
Alois Reitbauer <a.reitbauer@gmail.com>
2023-07-04 16:50:13 +0200

    very simple feature flag in app
    Signed-off-by: Alois Reitbauer <a.reitbauer@gmail.com>

--- demoapp/app.js
+++ demoapp/app.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const OpenFeature = require('@openfeature/js-sdk').OpenFeature;
+const FlagdProvider = require('@openfeature/flagd-provider').FlagdProvider;
+
+/**
+ * OpenFeature init code
+ */
+
+OpenFeature.setProvider(new FlagdProvider({
+    host: 'localhost',
+    port: 8013,
+}))
+
+const featureFlags = OpenFeature.getClient();
+
+/**
+ * Minimal application code
+ */
+
+app.get('/', async(req, res) => {
+  const myFlag = await featureFlags.getBooleanValue('my-flag', false);
+  if (myFlag){
+    res.send('+++ Hello World! +++ ')
+  } else {
+    res.send('Hello World!')
+  }
 })
 
 app.listen(port, () => {
