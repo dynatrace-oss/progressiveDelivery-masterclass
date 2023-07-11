dd728e7cb792c95a5a459017448254d9f5b744c7
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-11 12:14:50 +1000

    inline command not cd

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-cd ../demoapp
 docker buildx create --use
-docker buildx build --platform linux/arm64,linux/amd64 --push -t aloisreitbauer/featuredemo:V1 -f ./Dockerfile .
+docker buildx build --platform linux/arm64,linux/amd64 --push -t aloisreitbauer/featuredemo:V1 -f ../demoapp/Dockerfile .
