cc9439577e543a218486cc65d64d5c805e85b8c7
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-07-11 12:07:37 +1000

    cd to correct dir before build

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker buildx build --platform linux/arm64,linux/amd64 --push -t aloisreitbauer/featuredemo:V1 -f ./demoapp/Dockerfile .
+cd ../demoapp
+docker buildx create --use
+docker buildx build --platform linux/arm64,linux/amd64 --push -t aloisreitbauer/featuredemo:V1 -f ./Dockerfile .
