2f9f3c6808ad19d79d88654f2a1dbbfbf6844345
Alois Reitbauer <a.reitbauer@gmail.com>
2023-07-11 09:24:56 +0200

    add a slowFlag and OpenTelemetry
    Signed-off-by: Alois Reitbauer <a.reitbauer@gmail.com>

--- demoapp/app.js
+++ demoapp/app.js
@@ -2,6 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/**
+ * OpenTelemetry Code
+ */
+
+// Require dependencies
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter()
+  }),
+  instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk
+  .start()
+
+/**
+ * OpenFeature relevant code
+ */
+
 const OpenFeature = require('@openfeature/js-sdk').OpenFeature;
 const FlagdProvider = require('@openfeature/flagd-provider').FlagdProvider;
 
@@ -9,9 +34,7 @@ const openFeatureConf = {
   HOST : process.env.FLAGD_HOST || 'localhost',
   PORT : process.env.FLAGD_PORT || '8031'
 }
-
 console.log ("Connecting to flagD at %s:%s", openFeatureConf.HOST, openFeatureConf.PORT)
-
 
 /**
  * OpenFeature init code
