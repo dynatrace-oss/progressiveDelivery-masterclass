2b2d11b0d3bddf6566ddd8b366022d92693445e3
Alois Reitbauer <a.reitbauer@gmail.com>
2023-07-12 11:19:39 +0200

    made the output a bit prettier
    Signed-off-by: Alois Reitbauer <a.reitbauer@gmail.com>

--- demoapp/npm-install.sh
+++ demoapp/npm-install.sh
@@ -3,3 +3,6 @@ npm install @opentelemetry/sdk-node \
   @opentelemetry/auto-instrumentations-node \
   @opentelemetry/sdk-metrics \
   @openfeature/js-sdk @opentelemetry/api @openfeature/open-telemetry-hook
+
+  npm install @openfeature/open-telemetry-hook
+  npm install @openfeature/js-sdk @opentelemetry/api
