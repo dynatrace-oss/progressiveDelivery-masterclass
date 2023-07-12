39a9d36dd67b0b952e8f7c87add947b09448e591
Alois Reitbauer <a.reitbauer@gmail.com>
2023-07-12 16:46:33 +0200

    updated to new image
    Signed-off-by: Alois Reitbauer <a.reitbauer@gmail.com>

--- demoapp/buildImages.sh
+++ demoapp/buildImages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 --push --tag aloisreitbauer/featuredemo:V1 .
+docker buildx build --platform linux/amd64,linux/arm64 --push --tag aloisreitbauer/featuredemo:V1.1 .
