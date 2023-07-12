cb4feaf849c0d266f504c8a917e87a2357b6ffdf
Giovanni Liva <this_that@hotmail.it>
2023-07-12 22:06:22 +0200

    fix js test
    Signed-off-by: Giovanni Liva <this_that@hotmail.it>

--- gitops/manifests/demo-application/load.js
+++ gitops/manifests/demo-application/load.js
@@ -2,7 +2,7 @@ import { sleep } from"k6";
 import http from "k6/http";
 
 export let options = {
-  duration: "30sm",
+  duration: "30m",
   vus: 50,
   thresholds: {
     http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
