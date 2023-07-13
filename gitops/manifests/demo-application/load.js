301dfa042e83cfb28649a78ade610f0875506313
Giovanni Liva <giovanni.liva@dynatrace.com>
2023-07-13 11:25:33 +0200

    remove threshold
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- gitops/manifests/demo-application/load.js
+++ gitops/manifests/demo-application/load.js
@@ -4,9 +4,6 @@ import http from "k6/http";
 export let options = {
   duration: "1m",
   vus: 50,
-  thresholds: {
-    http_req_duration: ["p(95)<500"] // 95 percent of response times must be below 500ms
-  }
 };
 
 export default function() {
