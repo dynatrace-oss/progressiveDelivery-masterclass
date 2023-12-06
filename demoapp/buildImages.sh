fd14ed8b235b9b52e41ae087b30ac3b25aaa204f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-12-06 15:46:48 +1000

    Delete demoapp directory

--- demoapp/buildImages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker buildx create --use
-docker buildx build --platform linux/amd64,linux/arm64 --push --tag dynatrace-oss/featuredemo:V1.1 .
--- demoapp/npm-install.sh
+++
@@ -1,8 +0,0 @@
-npm install @opentelemetry/sdk-node \
-  @opentelemetry/api \
-  @opentelemetry/auto-instrumentations-node \
-  @opentelemetry/sdk-metrics \
-  @openfeature/js-sdk @opentelemetry/api @openfeature/open-telemetry-hook
-
-  npm install @openfeature/open-telemetry-hook
-  npm install @openfeature/js-sdk @opentelemetry/api
